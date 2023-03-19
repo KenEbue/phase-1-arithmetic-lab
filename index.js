23c46c64233d88cb77972a8be345b60c8a398825
Kenneth <kenneth.ebue@gmail.com>
2023-03-19 12:55:41 -0700

    Done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.round(Math.random()) +1; 
+
+let num3 = 12
+let num4 = 8
+let mod = 12 % 8
+
+let max = Math.ceil(20)
